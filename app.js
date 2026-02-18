b0bef2db404c9513d49389c40419de4b9bc26369
shubham99k <ssali71173@gmail.com>
2026-02-18 15:58:58 +0530

    add final touches

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
+const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 const User = require("./models/user.js");
 const passport = require("passport");
@@ -24,7 +25,11 @@ const userRouter = require("./routes/user.js");
 
 
 // DB
-mongoose.connect("mongodb://127.0.0.1:27017/wanderlust")
+
+// MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
+mongoose.connect(dbUrl)
     .then(() => console.log("Connected to DB"))
     .catch(err => console.log(err));
 
@@ -39,15 +44,29 @@ app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", (err) => {
+    console.log("Error in Mongo session store:", err);
+});
+
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
-        cookie: {
-            expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-            httpOnly: true,
-        }
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    }
 }
 
 app.use(session(sessionOptions));
@@ -68,9 +87,9 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
-app.get("/", (req, res) => {
-    res.send("Hi, I am root");
-});
+// app.get("/", (req, res) => {
+//     res.send("Hi, I am root");
+// });
 
 // routers added
 app.use("/listings", listingRouter);
